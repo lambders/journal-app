52027ec6b31335cec93aa8966e412e7b4c60fe23
Amanda Vu <amanda321@berkeley.edu>
2022-07-05 15:30:53 -0700

    looking pretty!

--- app/src/main/java/com/example/jour/MainActivity.java
+++ app/src/main/java/com/example/jour/MainActivity.java
@@ -2,22 +2,17 @@ package com.example.jour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -37,9 +32,14 @@ public class MainActivity extends AppCompatActivity {
         int num_prompts = prompts.size();
         Log.e("Number of prompts", String.valueOf(num_prompts));
 
+        // Set initial prompt
+        TextView prompt_view = findViewById(R.id.prompt);
+        final int randomNum = ThreadLocalRandom.current().nextInt(0, num_prompts);
+        String random_prompt = prompts.get(randomNum);
+        prompt_view.setText(random_prompt);
+
         // Serve new prompt
         FloatingActionButton refresh = findViewById(R.id.refresh);
-        TextView prompt_view = findViewById(R.id.prompt);
         refresh.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,12 +53,12 @@ public class MainActivity extends AppCompatActivity {
     private List<String> getPrompts(String fileName) {
         List<String> lines = new ArrayList<String>();
         try {
-            InputStream stream = null;
+            InputStream stream;
             stream = getAssets().open(fileName);
             BufferedReader reader = null;
             try {
                 reader = new BufferedReader(new InputStreamReader(stream));
-                String line = null;
+                String line;
                 while ((line = reader.readLine()) != null) {
                     lines.add(line);
                 }
